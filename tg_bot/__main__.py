d35c6f1fd11fe8366c91b7138692e714d71255ea
DarkLord1731 <siddhantnaik17@gmail.com>
2018-09-08 15:13:36 +0530

    Update main message

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -19,15 +19,10 @@ from tg_bot.modules.helper_funcs.chat_status import is_user_admin
 from tg_bot.modules.helper_funcs.misc import paginate_modules
 
 PM_START_TEXT = """
-Hi {}, my name is {}! If you have any questions on how to use me, read /help - and then head to @MarieSupport.
+Hi *{}*, my name is *{}*! If you have any questions on how to use me, read /help.
 
-I'm a group manager bot maintained by [this wonderful person](tg://user?id={}). I'm built in python3, using the \
-python-telegram-bot library, and am fully opensource - you can find what makes me tick \
-[here](github.com/PaulSonOfLars/tgbot)!
-
-Feel free to submit pull requests on github, or to contact my support group, @MarieSupport, with any bugs, questions \
-or feature requests you might have :)
-I also have a news channel, @MarieNews for announcements on new features, downtime, etc.
+I'm a group manager bot maintained by [Siddhant Naik](tg://user?id={}). I'm built in python3, using the \
+python-telegram-bot library, and am fully opensource [here](github.com/DarkLord1731/neo8_bot)!
 
 You can find the list of available commands with /help.
 
@@ -40,10 +35,10 @@ I'm a modular group management bot with a few fun extras! Have a look at the fol
 the things I can help you with.
 
 *Main* commands available:
- - /start: start the bot
+ - /start: Starts the bot
  - /help: PM's you this message.
  - /help <module name>: PM's you info about that module.
- - /donate: information about how to donate!
+ - /donate: Shows information about how to donate me!
  - /settings:
    - in PM: will send you your settings for all supported modules.
    - in a group: will redirect you to pm, with all that chat's settings.
@@ -53,10 +48,10 @@ And the following:
 """.format(dispatcher.bot.first_name, "" if not ALLOW_EXCL else "\nAll commands can either be used with / or !.\n")
 
 DONATE_STRING = """Heya, glad to hear you want to donate!
-It took lots of work for [my creator](t.me/SonOfLars) to get me to where I am now, and every donation helps \
-motivate him to make me even better. All the donation money will go to a better VPS to host me, and/or beer \
-(see his bio!). He's just a poor student, so every little helps!
-There are two ways of paying him; [PayPal](paypal.me/PaulSonOfLars), or [Monzo](monzo.me/paulnionvestergaardlarsen)."""
+It took lots of work for [my creator](t.me/SiddhantNaik1731) to get me to where I am now, and every donation helps \
+motivate him to make me even better. All the donation money will go to a better VPS to host me. \
+He's just a poor student, so every little helps!
+Donate through [PayPal](paypal.me/SiddhantNaik)."""
 
 IMPORTED = {}
 MIGRATEABLE = []
