67d2ea21c11f8083f85ff35b2ef2f04475d9e3c5
DarkLord1731 <siddhantnaik17@gmail.com>
2018-09-08 15:04:21 +0530

    Update default welcome and goodbye replies

--- tg_bot/modules/sql/welcome_sql.py
+++ tg_bot/modules/sql/welcome_sql.py
@@ -5,8 +5,8 @@ from sqlalchemy import Column, String, Boolean, UnicodeText, Integer, BigInteger
 from tg_bot.modules.helper_funcs.msg_types import Types
 from tg_bot.modules.sql import SESSION, BASE
 
-DEFAULT_WELCOME = "Hey {first}, how are you?"
-DEFAULT_GOODBYE = "Nice knowing ya!"
+DEFAULT_WELCOME = "Welcome to this chat {first}"
+DEFAULT_GOODBYE = "BYE!"
 
 
 class Welcome(BASE):
